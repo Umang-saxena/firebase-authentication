1ee1588414d3944a8aca8f54a3ee2b51512ba1a2
Umang Saxena <hanusaxena68@gmail.com>
2025-04-12 00:57:51 +0530

    Template working

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Login from './components/Login'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="row">
+      <Login />
+    </div>
+      
+    </>
+  )
+}
+
+export default App
